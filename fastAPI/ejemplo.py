4f5353faaf6ee696132974b2b0ca75f49d9f9935
Iparigl784 <iparigl784@g.educaand.es>
2025-11-24 21:14:07 +0100

    Ejercicios de ejemplo con python

---
+++ fastAPI/ejemplo.py
@@ -0,0 +1,4 @@
+nombre: str = "Ivan"
+telefono: int = 123456789
+
+print(f"Nombre: {nombre}, Teléfono: {telefono}")
---
+++ fastAPI/perfil.py
@@ -0,0 +1,9 @@
+# Usando una sola f-string y print(): muestra un resumen de todas las variables. Ejemplo:
+#--- Perfil de usuario --- Nombre: Ivan, Edad: 30, Ciudad: Madrid, Estudiante: False
+
+nombre: str = "Ivan"
+edad: int = 30
+ciudad: str = "Madrid"
+es_estudiante: bool = False
+
+print(f"--- Perfil de usuario --- Nombre: {nombre}, Edad: {edad}, Ciudad: {ciudad}, Estudiante: {es_estudiante}")
